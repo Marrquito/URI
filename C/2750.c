f6cd9ef63acf9a08a19a2591f64c4ed95c9c4aa6
Marrquito <marcossetin@hotmail.com>
2021-04-12 17:18:36 -0300

    Problemas resolvidos em C

---
+++ C/2750.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q , r;
+    a = 0;
+    b = 1;
+    c = 2;
+    d = 3;
+    e = 4;
+    f = 5;
+    g = 6;
+    h = 7;
+    i = 8; 
+    j = 9;
+    k = 10; 
+    l = 11; 
+    m = 12;
+    n = 13; 
+    o = 14;
+    p = 15;
+    q = 16;
+    r = 17;
+
+    printf("---------------------------------------\n");
+    printf("|  decimal  |  octal  |  Hexadecimal  |\n");
+    printf("---------------------------------------\n");
+    printf("|      %d    |    %d    |       %d       |\n", a, a, a);
+    printf("|      %d    |    %d    |       %d       |\n", b, b, b);
+    printf("|      %d    |    %d    |       %d       |\n", c, c, c);
+    printf("|      %d    |    %d    |       %d       |\n", d, d, d);
+    printf("|      %d    |    %d    |       %d       |\n", e, e, e);
+    printf("|      %d    |    %d    |       %d       |\n", f, f, f);
+    printf("|      %d    |    %d    |       %d       |\n", g, g, g);
+    printf("|      %d    |    %d    |       %d       |\n", h, h, h);
+    printf("|      %d    |   %2d    |       %d       |\n", i, k, i);
+    printf("|      %d    |   %2d    |       %d       |\n", j, l, j);
+    printf("|     %2d    |   %2d    |       A       |\n", k, m);
+    printf("|     %2d    |   %2d    |       B       |\n", l, n);
+    printf("|     %2d    |   %2d    |       C       |\n", m, o);
+    printf("|     %2d    |   %2d    |       D       |\n", n, p);
+    printf("|     %2d    |   %2d    |       E       |\n", o, q);
+    printf("|     %2d    |   %2d    |       F       |\n", p, r);
+    printf("---------------------------------------\n");
+
+    return 0;
+}
